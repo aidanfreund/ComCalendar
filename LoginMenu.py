80b072995798934ee006c81b48504ea46c4eac44
Justin Grage <jdgrage@ilstu.edu>
2024-10-29 14:48:24 -0500

    Made changes to InputController and LoginMenu

---
+++ LoginMenu.py
@@ -0,0 +1,15 @@
+# Entry point of program
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+
+class LoginMenu:
+    def _login(self):
+        pass
+    def _create_profile(self):
+        pass
+
