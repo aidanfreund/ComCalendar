9b090b9461d07b95ee378e744c8fc6fa6178df10
Da18sh <dashpfeifer@aol.com>
2024-10-29 14:45:06 -0500

    made nonpublic

--- Calendar.py
+++ Calendar.py
@@ -1,43 +1,43 @@
 class Calendar:
-    def __init__(self, _CalendarID, _CalendarName, _Events, _Tasks):
-        self.CalendarID = _CalendarID
-        self.CalendarName = _CalendarName
-        self.Events = _Events if _Events is not None else []
-        self.Tasks = _Tasks if _Tasks is not None else []
+    def __init__(self, CalendarID, CalendarName, Events, Tasks):
+        self._CalendarID = CalendarID
+        self._CalendarName = CalendarName
+        self._Events = Events if Events is not None else []
+        self._Tasks = Tasks if Tasks is not None else []
 
     def GetCalendarID(self):
-        return self.CalendarID
+        return self._CalendarID
     
     def SetCalendar(self, CalendarID):
-        self.CalendarId = CalendarID
+        self._CalendarId = CalendarID
 
     def GetCalendarName(self):
-        return self.CalendarName
+        return self._CalendarName
 
     def SetCalendarName(self, CalendarName):
-        self.CalendarName = CalendarName
+        self._CalendarName = CalendarName
 
     def RetrieveTasks(self):
-        return self.Tasks
+        return self._Tasks
     
     def RetrieveEvents(self):
-        return self.Events
+        return self._Events
     
     def AddTask(self, Task):
-        self.Tasks.append(Task)
+        self._Tasks.append(Task)
     
     def AddEvent(self, Event):
-        self.Events.append(Event)
+        self._Events.append(Event)
 
     def DeleteTask(self, hapID):
         try:
-            self.Tasks.remove(hapID)
+            self._Tasks.remove(hapID)
         except ValueError:
             print(f"Task ID '{hapID}' not found." )
 
     def DeleteEvent(self, hapID):
         try:
-            self.Tasks.remove(hapID)
+            self._Tasks.remove(hapID)
         except ValueError:
             print(f"Event Id '{hapID}' not found.")
 
