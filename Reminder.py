4a927883e7e8adc6cf257cfc3530eb76b5bf4b93
Edwin Chavez <edwinicars2014@gmail.com>
2024-10-29 14:18:02 -0500

    Added Reminder and Hapenning Classes

---
+++ Reminder.py
@@ -0,0 +1,26 @@
+#Reminder class for Calendar
+#Contributers: Edwin Chavez
+
+import datetime
+
+class Reminder:
+
+    int(__reminder_id)
+
+    datetime(__time)
+
+    #Returns the Unique ReminderID
+    def get_reminder_id():
+        return __reminder_id
+    #Returns the Date and Time of the Reminder
+    def get_time():
+        return __time
+    
+    #Sets the Date and Time for the Reminder
+    def set_time(self, date):
+        self.date = date.date
+        self.time = date.time
+
+
+    
+
