5ecc8db94c42bde93440249c49250ba5c3249d85
Justin Grage <jdgrage@ilstu.edu>
2024-12-05 13:08:10 -0600

    Updates parameters to Operator and LoginMenu

--- Operator.py
+++ Operator.py
@@ -1,104 +1,148 @@
 from Profile import Profile
-from FactoryProducer import FactoryProducer
+from Calendar import Calendar
+from DBFactory import FactoryProducer, Database_factory 
+from DBProfile import DB_Profile
+from DBConnection import Database_Connection
+from CalendarFilter import CalendarFilter
+
+
 
 class Operator:
 
+    factory:Database_factory = FactoryProducer("Profile")
+    db_connection:Database_Connection = factory.DB_Connection
+    db_profile:DB_Profile = factory.DB_profile
+
+    if db_connection is None:
+        raise Exception('Failed to connect to database')
+
+    # Creates new event with attributes, returns true if successful
     @classmethod
-    def add_event(cls, event_id, name, start_time, end_time, calendar):
+    def add_event(cls, name, start_time, end_time, calendar_obj):
+        DB_Profile.add_event()
+
+    # Edits event, returns true if successful
+    @classmethod
+    def edit_event(cls, name, start_time, end_time, event_obj):
+        pass
+    
+    # Deletes event from calendar, returns true if successful
+    @classmethod
+    def delete_event(cls, event_obj, profile_obj):
+        pass
+    
+    # Creates new calendar with name, returns true if successful
+    @classmethod
+    def create_calendar(cls, name, profile_obj):
+        pass
+    
+    # Deletes calendar 
+    @classmethod
+    def delete_calendar(cls, calendar_obj, profile_obj):
         pass
 
+    # Processes .ics file string and returns calendar object
     @classmethod
-    def edit_event(cls, event_id, new_name, new_description, start_time, end_time, calendar):
+    def upload_calendar(cls, calendar, profile):
         pass
-
-    @classmethod
-    def delete_event(cls, event_id, calendar):
-        pass
-
-    @classmethod
-    def create_calendar(cls, calendar_id, name):
-        pass
-
-    @classmethod
-    def delete_calendar(cls, calendar_id):
-        pass
-
-    @classmethod
-    def upload_calendar(cls, calendar):
-        pass
-
+    
+    # Returns .ics file string 
     @classmethod
     def download_calendar(cls):
         pass
 
+    
+    # Combines calendars, returns a new calendar with combined objects
     @classmethod
-    def aggregate_calendar(cls, calendar1, calendar2):
+    def aggregate_calendar(cls, calendar_obj1, calendar_obj2):
         pass
 
+    # Adds reminder to happening obj, returns true if successful
     @classmethod
-    def create_reminder(cls, reminder_id, time):
+    def create_reminder(cls, start_time, happ_obj):
         pass
 
+    # Returns calendar obj from db using calendar_id
     @classmethod
     def retrieve_calendar(cls, calendar_id):
         pass
 
+    # To be discussed
     @classmethod
-    def filter_calendar_by_events(cls, calendar, start_time, end_time):
+    def retrieve_event_information(cls, event_id, calendar_obj):
         pass
 
+    # To be discussed
     @classmethod
-    def retrieve_event_information(cls, event_id, calendar):
+    def retrieve_task_information(cls, task_id, calendar_obj):
         pass
 
+     # Filters calendar by events, returns a filtered calendar obj
     @classmethod
-    def retrieve_task_information(cls, task_id, calendar):
-        pass
+    def filter_calendar_by_events(cls, calendar_obj, start_date, end_date):
+        return CalendarFilter.FilterByDates(calendar_obj,start_date,end_date)
 
+
+    # Filters calendar by tasks, returns a filtered calendar obj
     @classmethod
-    def filter_calendar_by_tasks(cls, calendar, start_time, end_time):
-        pass
-
-    @classmethod
-    def add_task(cls, task_id, description, due_date, calendar):
-        pass
-
-    @classmethod
-    def remove_task(cls, task_id, calendar):
-        pass
-
-    @classmethod
-    def edit_task(cls, task_id, new_name, new_description, due_date, calendar):
-        pass
-
-    @classmethod
-    def copy_calendar(cls, calendar_id):
-        pass
-
-    @classmethod
-    def compare_calendars(cls, calendar_id1, calendar_id2):
-        pass
-
-    @classmethod
-    def remove_reminder(cls, reminder_id):
-        pass
-
-    @classmethod
-    def change_reminder(cls, reminder_id, new_time):
-        pass
-
-    @classmethod
-    def delete_profile(cls, profile_id):
-        pass
-
+    def filter_calendar_by_tasks(cls, calendar_obj, start_date, end_date):
+        return CalendarFilter.FilterByDates(calendar_obj,start_date,end_date)
+    
+     # Filters calendar by dates, returning a new filtered calendar obj
     @classmethod
     def filter_by_dates(cls, calendar, start_date, end_date):
         pass
 
+    # Adds task to a calendar, returns true if successful
     @classmethod
-    def validate_login(cls,username, password):
+    def add_task(cls, description, due_date, calendar_obj):
         pass
 
+    # Removes an task from calendar, returns true if successful
+    @classmethod
+    def remove_task(cls, task_obj, calendar_obj):
+        pass
+
+    # Edits a task, returns true if successful
+    @classmethod
+    def edit_task(cls, description, due_date, task_obj):
+        pass
+
+    # Copies a calendar and adds it to profile, returns true if successful
+    @classmethod
+    def copy_calendar(cls, calendar_obj, profile_obj):
+        pass
+
+    # Compares calendars in a given time frame, returns string of *(conflicts or free space?)
+    @classmethod
+    def compare_calendars(cls, calendar_id1, calendar_id2, start_time, end_time):
+        pass
+    
+    # Removes reminder, returns true if successful
+    @classmethod
+    def remove_reminder(cls, reminder_obj, happ_obj):
+        pass
+
+    # Edits a reminder object, returns true if successful
+    @classmethod
+    def edit_reminder(cls, reminder_id, new_time):
+        pass
+
+    # Deletes profile obj, returns true if successful
+    @classmethod
+    def delete_profile(cls, profile_obj):
+        pass
+
+   
+    #Logs in to profile using username and password
+    #Returns profile obj if a match exists
+    @classmethod
+    def login(cls,username, password):
+        return cls.db_profile.read_profile(username,password,cls.db_connection)
+
+
+    #Creates a profile obj and returns it 
     @classmethod
     def create_profile(cls, username, password):
-        pass
+        return cls.db_profile.add_profile(username,password,cls.db_connection)
+        
