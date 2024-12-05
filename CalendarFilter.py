d6f7519b9f90be2c35d1a7ab1273347286bb8cac
Aidan Freund <132619139+aidanfreund@users.noreply.github.com>
2024-12-05 13:02:55 -0600

    CalendarFilter.py

--- CalendarFilter.py
+++ CalendarFilter.py
@@ -1,30 +1,10 @@
 from Calendar import Calendar
-import datetime
-
-class CalendarFilter():
-    def __init__(self, calendar:Calendar):
-        self._calendar = calendar
-        
+class CalendarFilter(Calendar):
+    def FilterByDates(self, start_time, end_time):
+        return any(start_time <= event['date'] <= end_time for event in self.events)
     
-    def filter_by_dates(self, start:datetime, end:datetime):
-        """returns a calendar excluding events and tasks that start outside of the given time range """
-        new_cal = Calendar(self._calendar.get_calendar_id(), self._calendar.get_calendar_name(), [], [])
-
-        for event in self._calendar.RetrieveEvents():
-            if (start <= event.get_first_time() <= end): # and second?
-                new_cal.add_event(event)
-
-        for task in self._calendar.RetrieveTasks():
-            if start <= task.get_first_time() <= end:
-                new_cal.add_task(task)
-
-        return new_cal
-
+    def FilterByTask(self, task):
+        return any(event['task'] == task for event in self.tasks)
     
-    def filter_by_tasks(self, task):
-        return self._calendar.retrieve_tasks()
-    
-    def filter_by_events(self, event):
-        return self._calendar.retrieve_events()
-    
-    
+    def FilterByEvent(self, event):
+        return any(thing['event'] == event for thing in self.events)
