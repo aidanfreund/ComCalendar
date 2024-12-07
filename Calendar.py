2708449ecfd5dc0f569a503c283ef026c6a63e02
Aidan Freund <132619139+aidanfreund@users.noreply.github.com>
2024-12-07 16:15:36 -0600

    Todays stuff sofar

--- Calendar.py
+++ Calendar.py
@@ -1,4 +1,7 @@
 from Happening import Happening
+from Task import Task
+from Event import Event
+from datetime import datetime
 
 class Calendar:
     def __init__(self, calendar_id, calendar_name, events, tasks):
@@ -13,15 +16,12 @@ class Calendar:
     
     def set_calendar(self, calendar_id):
         self._calendar_id = calendar_id
-        return True
 
     def get_calendar_name(self):
-
         return self._calendar_name
 
     def set_calendar_name(self, calendar_name):
         self._calendar_name = calendar_name
-        return True
 
     def retrieve_tasks(self):
         return self._tasks
@@ -29,16 +29,18 @@ class Calendar:
     def retrieve_events(self):
         return self._events
     
-    def add_task(self, task):
+    def add_task(self, id:int, time:datetime, name:str, desc:str):
+        task = Task(id, name, time, desc)
         self._tasks.append(task)
         return True
     
-    def add_event(self, event):
+    def add_event(self, id:int, name:str, time1:datetime, time2:datetime, desc:str):
+        event = Event(id, name, time1, time2, desc)
         self._events.append(event)
         return True
   
     #removes task from task array. using the id associated with task
-    def delete_task(self, hap_id):
+    def delete_task(self, hap_id:int):
         try:
             for task in self._tasks:
                 if task.get_id() == hap_id:
@@ -49,7 +51,7 @@ class Calendar:
             return False
           
     #removes event from event array. using the id associated with event
-    def delete_event(self, hap_id):
+    def delete_event(self, hap_id:int):
         try:
             for event in self._events:
                 if event.get_id() == hap_id:
--- Operator.py
+++ Operator.py
@@ -1,45 +1,137 @@
 from Profile import Profile
+from DBProfile import DBProfile
+from Calendar import Calendar
+from DBFactory import DatabaseFactory, FactoryProducer
+from DBConnection import DatabaseConnection
+from datetime import timezone, datetime
 
+from Profile import Profile
+from Calendar import Calendar
+from Event import Event
+from Task import Task
 
+from ics import Calendar as ICS_Calendar
+from ics import Event as ICS_Event
+import os
 
 class Operator:
 
-    factory:Database_factory = FactoryProducer("Profile")
-    db_connection:Database_Connection = factory.DB_Connection
-    db_profile:DB_Profile = factory.DB_profile
+    factory:DatabaseFactory = FactoryProducer("Profile")
+    db_profile:DBProfile = factory.DB_profile
 
-    if db_connection is None:
-        raise Exception('Failed to connect to database')
 
-    # Creates new event with attributes, returns true if successful
+    #Creates new event with attributes, returns true if successful
     @classmethod
-    def add_event(cls, name, start_time, end_time, calendar_obj):
-        pass
+    def add_event(cls, name, start_time, end_time, calendar_obj:Calendar, description = ""):
+        #add to db
+        id = cls.db_profile.add_event(description, start_time, end_time, name, calendar_obj)
+        #add to machine
+        calendar_obj.add_event(id, name, start_time, end_time, description)
+        return True
 
     # Edits event, returns true if successful
     @classmethod
-    def edit_event(cls, name, start_time, end_time, event_obj):
-        pass
+    def edit_event(cls, name:str, start_time:datetime, end_time:datetime, event_obj:Event, desc:str):
+        #update on machine
+        event_obj.set_name(name)
+        event_obj.set_first_time(start_time)
+        event_obj.set_second_time(end_time)
+        event_obj.set_description(desc)
+        #attempt update in db
+        return cls.db_profile.change_event(event_obj)
     
     # Deletes event from calendar, returns true if successful
     @classmethod
-    def delete_event(cls, event_obj, profile_obj):
-        pass
+    def delete_event(cls, event_obj:Event, calendar_obj:Calendar):
+        #delete from db
+        db = cls.db_profile.delete_event(event_obj)
+        #delete from machine
+        mach = calendar_obj.delete_event(event_obj.get_id())
+
+        return (db and mach)
     
     # Creates new calendar with name, returns true if successful
     @classmethod
-    def create_calendar(cls, name, profile_obj):
-        pass
+    def create_calendar(cls, name, profile_obj:Profile):
+        #check amount of calendars assosciated with profile, ensure its less than 6(the max)
+        if len(profile_obj.get_calendars())<6:
+            #create in db
+            id = cls.db_profile.add_calendar(name, profile_obj)
+            #create on machine
+            return profile_obj.create_new_calendar(id, name)
+
+        else:
+            print("Profile may only have 6 calendars, delete one and try again")
+            return False
     
     # Deletes calendar 
     @classmethod
-    def delete_calendar(cls, calendar_obj, profile_obj):
+    def delete_calendar(cls, calendar_obj:Calendar, profile_obj:Profile):
+        #
+        cls.db_profile.delete_calendar(calendar_obj)
+        #remove from profile array to delete
+        profile_obj.delete_calendar(calendar_obj)
         pass
 
-    # Processes .ics file string and returns calendar object
+    # Processes .ics file string and returns a boolean indicating its success
     @classmethod
-    def upload_calendar(cls, calendar_obj, profile_obj):
-        pass
+    def upload_calendar(cls, name:str, file_path:str, profile_obj:Profile):
+        """reads an ICS file to create a calendar obj.  provided a file path to read from, 
+        a name for the calendar, and the profile obj to be added to.
+        will return calendar object. assumes user is in UTC time zone 
+        (in order to reduce required downloads)"""
+        #first ensure profile has no more than 5 calendars already
+        if len(profile_obj.get_calendars())<6:
+            #create in db
+            id = cls.db_profile.add_calendar(name, profile_obj)
+            #create on machine
+            calendar_made = profile_obj.create_new_calendar(id, name)
+        else:
+            print("Profile may only have 6 calendars, delete one and try again")
+            return False
+        
+        #validate file
+        #open path and read
+        if not os.path.exists(file_path):
+            print(f"Error: File path '{file_path}' does not exist.")
+            return False
+        if not file_path.endswith(".ics"):
+            print(f"Error: File path '{file_path}' is not an ICS file.")
+            return False
+        with open(file_path, 'r') as f:
+            ics_content = f.read()
+        ics_calendar = ICS_Calendar(ics_content)
+
+
+        if calendar_made:
+            #find current time, and convert to utc
+            calendar = Calendar(id, "temp", [], [])
+            today = datetime.now()
+            today = today.replace(tzinfo=timezone.utc)
+            # Iterate through events in the .ics file and add them to the Calendar
+            for ics_event in ics_calendar.events:
+                desc = ics_event.name
+                start = ics_event.begin
+                end = ics_event.end
+
+                # Create an object and add it to the Calendar
+                # ics only has events, if start and end time are the same, interperate the event as a task
+                
+                # if start != end:
+                #     id = cls.db_profile.add_event(desc, start, end, name, cls.db_profile.))
+                #     calendar.add_event(id, )
+                # else:
+                #     task = Task(hap_id, desc, start)
+                #     #assume tasks in past are completed
+                #     if start < today:
+                #         task.set_completed(False)
+                    #calendar.add_task(task)
+
+                #update db calendar
+
+            return True
+        else:
+            return False
     
     # Returns .ics file string 
     @classmethod
@@ -53,13 +145,47 @@ class Operator:
 
     # Compares calendars in a given time frame, returns string of *(conflicts or free space?)
     @classmethod
-    def compare_calendars(cls, calendar_obj1, calendar_obj2, start_time, end_time):
-        pass
+    def compare_calendars(cls, calendar_id1, calendar_id2):
+
+  
+        # cals = Profile.get_calendars()
+
+        # for calendar in cals:
+        #     if calendar.get_id() ==calendar_id1:
+        #         events += calendar.get_events()
+        #     elif calendar.get_id() ==calendar_id2:
+        #         events += calendar.get_events()
+        # min_time = datetime.now()
+        # max_time = 0
+        # freetime = ""
+        # for event in events:
+        #     first = event.get_first_time()
+            
+        return 
     
     # Combines calendars, returns a new calendar with combined objects
     @classmethod
-    def aggregate_calendar(cls, calendar_obj1, calendar_obj2):
-        pass
+    def aggregate_calendar(cls,name:str, calendar_id1:int, calendar_id2:int):
+#work required
+#i will come back to
+        cls.db_profile.add_calendar(name, cls.db_profile.read_profile())
+
+        new_id = -1
+        events= []
+        tasks = []
+        # cals = Profile.get_calendars()
+        
+        # #find calendars and add their happenings to new calendar:
+        # for calendar in cals:
+        #     if calendar.get_id() ==calendar_id1:
+        #         events += calendar.get_events()
+        #         tasks += calendar.get_tasks()
+        #     elif calendar.get_id() ==calendar_id2:
+        #         events += calendar.get_events()
+        #         tasks += calendar.get_tasks()
+        agg_calendar = Calendar(new_id, name, events, tasks)
+
+        return agg_calendar
 
     # Adds reminder to happening obj, returns true if successful
     @classmethod
