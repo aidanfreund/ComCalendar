d39ccc74267840d54aaa6448b2058d267179b0ca
Da18sh <dashpfeifer@aol.com>
2024-12-07 19:38:45 -0600

    Calendar changes

--- Calendar.py
+++ Calendar.py
@@ -31,21 +31,15 @@ class Calendar:
     
     def add_task(self, id:int, time:datetime, name:str, desc:str):
         task = Task(id, name, time, desc)
-        print(f"adding task: {task.get_first_time()}")
         for i, etask in enumerate(self._tasks):
-            print(f"Comparing {time} with {etask}")
             if time < etask:
-                print(f"inserting at {i}")
                 self._tasks.insert(i, task.get_first_time())
                 return True
-        print("appended to end")
         self._tasks.append(task.get_first_time())
         return True
     def add_event(self, id:int, name:str, time1:datetime, time2:datetime, desc:str):
         event = Event(id, name, time1, time2, desc)
-        print(f"adding event: {event.get_second_time()}")
         for i, e_event in enumerate(self._events):
-            print(f"Comparing {time1} with {e_event}")
             if time1 < e_event:
                 self._events.insert(i, event.get_second_time())
                 return True
@@ -74,8 +68,4 @@ class Calendar:
             print(f"Task ID '{hap_id}' not found." )
             return False
         
-    def __str__(self):
-        task_string = [str(task) for task in self._tasks]
-        event_string = [str(event) for event in self._events]
-        return f"Claendar '{self._calendar_name}' :\nTasks:\n" + "\n".join(task_string)+"\nEvents:\n"+ "\n".join(event_string) 
-        
+
