6c025174de280da5311c4e0d52ce13564f1068e7
Da18sh <dashpfeifer@aol.com>
2024-12-07 19:37:16 -0600

    Commit to branch

--- Calendar.py
+++ Calendar.py
@@ -16,14 +16,12 @@ class Calendar:
     
     def set_calendar(self, calendar_id):
         self._calendar_id = calendar_id
-        return True
 
     def get_calendar_name(self):
         return self._calendar_name
 
     def set_calendar_name(self, calendar_name):
         self._calendar_name = calendar_name
-        return True
 
     def retrieve_tasks(self):
         return self._tasks
@@ -31,14 +29,31 @@ class Calendar:
     def retrieve_events(self):
         return self._events
     
-    def add_task(self, hap_id, name, datetime):
-        self._tasks.append(hap_id)
-    
-    def add_event(self, hap_id, name, start_time, end_time ):
-        self._events.append(hap_id)
-        
-
-    def delete_task(self, hap_id):
+    def add_task(self, id:int, time:datetime, name:str, desc:str):
+        task = Task(id, name, time, desc)
+        print(f"adding task: {task.get_first_time()}")
+        for i, etask in enumerate(self._tasks):
+            print(f"Comparing {time} with {etask}")
+            if time < etask:
+                print(f"inserting at {i}")
+                self._tasks.insert(i, task.get_first_time())
+                return True
+        print("appended to end")
+        self._tasks.append(task.get_first_time())
+        return True
+    def add_event(self, id:int, name:str, time1:datetime, time2:datetime, desc:str):
+        event = Event(id, name, time1, time2, desc)
+        print(f"adding event: {event.get_second_time()}")
+        for i, e_event in enumerate(self._events):
+            print(f"Comparing {time1} with {e_event}")
+            if time1 < e_event:
+                self._events.insert(i, event.get_second_time())
+                return True
+        self._events.append(event.get_second_time())
+        return True
+  
+    #removes task from task array. using the id associated with task
+    def delete_task(self, hap_id:int):
         try:
             for task in self._tasks:
                 if task.get_id() == hap_id:
@@ -46,12 +61,21 @@ class Calendar:
             return True
         except ValueError:
             print(f"Task ID '{hap_id}' not found." )
-
-    def delete_event(self, hap_id):
+            return False
+          
+    #removes event from event array. using the id associated with event
+    def delete_event(self, hap_id:int):
         try:
             for event in self._events:
                 if event.get_id() == hap_id:
-                    self._tasks.remove(hap_id)
+                    self._events.remove(event)
+            return True
         except ValueError:
             print(f"Task ID '{hap_id}' not found." )
             return False
+        
+    def __str__(self):
+        task_string = [str(task) for task in self._tasks]
+        event_string = [str(event) for event in self._events]
+        return f"Claendar '{self._calendar_name}' :\nTasks:\n" + "\n".join(task_string)+"\nEvents:\n"+ "\n".join(event_string) 
+        
